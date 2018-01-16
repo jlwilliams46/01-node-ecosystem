7394564ee09da021c02e0693b85409bdd0d0de74
Jamie Williams <jwilliams492@gmail.com>
2018-01-15 22:03:29 -0800

    completed arimethic.js code

--- lab-jamie/lib/arithmetic.js
+++ lab-jamie/lib/arithmetic.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const arithmetic = module.exports = {};
+
+arithmetic.add = function(fir, sec) {
+return null ? typeof fir !== 'number' || typeof sec !== 'number' : undefined; 
+return fir + sec ? typeof fir === 'number' || typeof sec === 'number' : undefined;
+};
+
+arithmetic.sub = function(fir, sec) {
+return null ? typeof fir !== 'number' || typeof sec !== 'number' : undefined; 
+return fir - sec ? typeof fir === 'number' || typeof sec === 'number' : undefined;
+};
