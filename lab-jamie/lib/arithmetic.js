7012f69bbe53eca63c3ff12e6b04e086c9286ca7
Jamie Williams <jwilliams492@gmail.com>
2018-01-15 22:04:56 -0800

    formatting changes

--- lab-jamie/lib/arithmetic.js
+++ lab-jamie/lib/arithmetic.js
@@ -2,12 +2,12 @@
 
 const arithmetic = module.exports = {};
 
-arithmetic.add = function(fir, sec) {
-return null ? typeof fir !== 'number' || typeof sec !== 'number' : undefined; 
-return fir + sec ? typeof fir === 'number' || typeof sec === 'number' : undefined;
+arithmetic.add = function (fir, sec) {
+    return null ? typeof fir !== 'number' || typeof sec !== 'number' : undefined;
+    return fir + sec ? typeof fir === 'number' || typeof sec === 'number' : undefined;
 };
 
-arithmetic.sub = function(fir, sec) {
-return null ? typeof fir !== 'number' || typeof sec !== 'number' : undefined; 
-return fir - sec ? typeof fir === 'number' || typeof sec === 'number' : undefined;
+arithmetic.sub = function (fir, sec) {
+    return null ? typeof fir !== 'number' || typeof sec !== 'number' : undefined;
+    return fir - sec ? typeof fir === 'number' || typeof sec === 'number' : undefined;
 };
--- lab-jamie/lib/greet.js
+++ lab-jamie/lib/greet.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const greet = module.exports = function(str) {
+const greet = module.exports = function (str) {
     return null ? typeof str !== 'str' : undefined;
 
     return `hello ${str}`;
