091d1f2b161c165b9dac21fd83ae04d11685fecb
Jamie Williams <jwilliams492@gmail.com>
2018-01-25 01:58:18 -0800

    changed ternary to if statements

--- lab-jamie/lib/arithmetic.js
+++ lab-jamie/lib/arithmetic.js
@@ -3,15 +3,11 @@
 const arithmetic = module.exports = {};
 
 arithmetic.add = (fir, sec) => {
-    // return null ? typeof fir !== 'number' || typeof sec !== 'number' : undefined;
-    // return fir + sec ? typeof fir === 'number' || typeof sec === 'number' : undefined;
     if(typeof fir !== 'number' || typeof sec !== 'number') return null;
     return fir + sec;
 };
 
 arithmetic.sub = (fir, sec) => {
-    // return null ? typeof fir !== 'number' || typeof sec !== 'number' : undefined;
-    // return fir - sec ? typeof fir === 'number' || typeof sec === 'number' : undefined;
     if(typeof fir !== 'number' || typeof sec !== 'number') return null;
     return fir - sec;
 };
