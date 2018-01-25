eb745ae0ac65cf7d8e1a6bda6285859ce7e55583
Jamie Williams <jwilliams492@gmail.com>
2018-01-25 00:38:16 -0800

    tests for proper and improper use

--- lab-jamie/__test__/greet.test.js
+++ lab-jamie/__test__/greet.test.js
@@ -2,3 +2,15 @@
 
 const greet = require('../lib/greet.js');
 require('jest');
+
+describe('Incorrect Input', () => {
+    it('should return null if input was not a string', () => {
+        expect(greet(3.14159265359).toBeNull());
+    })
+});
+
+describe('Correct Output', () => {
+    it('should take a string and concatenate it to the "hello"', () => {
+        expect(greet.hello('awesomeness').toEqual('hello awesomeness'));
+    })
+});
