494d8039636d91ecd99a00f9c1d5f8235967f6af
Jamie Williams <jwilliams492@gmail.com>
2018-01-25 01:59:22 -0800

    all tests passing

--- lab-jamie/__test__/arithmetic.test.js
+++ lab-jamie/__test__/arithmetic.test.js
@@ -1,3 +1,26 @@
 'use strict';
 
 const arithmetic = require('../lib/arithmetic.js');
+
+describe('Incorrect Input', () => {
+    it('should return null if either input is not a number', () => {
+        expect(arithmetic.add(42, 'phineas and ferb')).toBeNull();
+    })
+});
+
+describe('Correct Output', () => {
+    it('should return the sum of any two input numbers', () => {
+        expect(arithmetic.add(101010, 42)).toEqual(101052);
+    })
+});
+describe('Incorrect Input', () => {
+    it('should return null if either input is not a number', () => {
+        expect(arithmetic.sub(101010)).toBeNull();
+    })
+});
+
+describe('Correct Output', () => {
+    it('should return the difference of any two input numbers', () => {
+        expect(arithmetic.sub(101010, 42)).toEqual(100968);
+    })
+});
--- lab-jamie/__test__/greet.test.js
+++ lab-jamie/__test__/greet.test.js
@@ -5,12 +5,12 @@ require('jest');
 
 describe('Incorrect Input', () => {
     it('should return null if input was not a string', () => {
-        expect(greet.hello(3.14159265359).toBeNull());
+        expect(greet.hello(42)).toBeNull();
     })
 });
 
 describe('Correct Output', () => {
     it('should take a string and concatenate it to the "hello"', () => {
-        expect(greet.hello('awesomeness').toEqual('hello awesomeness'));
+        expect(greet.hello('awesomeness')).toEqual('hello awesomeness');
     })
 });
