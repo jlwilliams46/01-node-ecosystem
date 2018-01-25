f1c5bf9c027590f396e89a4be40bbfafa4e4eb31
Jamie Williams <jwilliams492@gmail.com>
2018-01-25 01:51:38 -0800

    finalized if statement instead of ternary operator

--- lab-jamie/lib/greet.js
+++ lab-jamie/lib/greet.js
@@ -3,7 +3,6 @@
 const greet = module.exports = {}; 
 
 greet.hello = (str) => {
-    // return null ? typeof str !== 'str' : undefined;
     if(typeof str !== 'string') return null;
 
     return `hello ${str}`;
