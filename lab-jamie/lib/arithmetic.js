737568212baa7fd0486d3563eb20c737bb867169
Jamie Williams <jwilliams492@gmail.com>
2018-01-25 01:52:47 -0800

    format change

--- lab-jamie/lib/arithmetic.js
+++ lab-jamie/lib/arithmetic.js
@@ -5,15 +5,13 @@ const arithmetic = module.exports = {};
 arithmetic.add = (fir, sec) => {
     // return null ? typeof fir !== 'number' || typeof sec !== 'number' : undefined;
     // return fir + sec ? typeof fir === 'number' || typeof sec === 'number' : undefined;
-    if(typeof fir !== 'number') return null;
-    if(typeof sec !== 'number') return null;
+    if(typeof fir !== 'number' || typeof sec !== 'number') return null;
     return fir + sec;
 };
 
 arithmetic.sub = (fir, sec) => {
     // return null ? typeof fir !== 'number' || typeof sec !== 'number' : undefined;
     // return fir - sec ? typeof fir === 'number' || typeof sec === 'number' : undefined;
-    if(typeof fir !== 'number') return null;
-    if(typeof sec !== 'number') return null;
+    if(typeof fir !== 'number' || typeof sec !== 'number') return null;
     return fir - sec;
 };
