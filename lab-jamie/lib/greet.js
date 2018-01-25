7eb4550ccf9cfb5e73d0dddd64af36b56a619cda
Jamie Williams <jwilliams492@gmail.com>
2018-01-25 00:09:17 -0800

    refactored

--- lab-jamie/lib/greet.js
+++ lab-jamie/lib/greet.js
@@ -1,7 +1,10 @@
 'use strict';
 
-const greet = module.exports = function (str) {
-    return null ? typeof str !== 'str' : undefined;
+const greet = module.exports {} 
+
+greet.hello = (str) => {
+    // return null ? typeof str !== 'str' : undefined;
+    if(typeof str !== 'string') return null;
 
     return `hello ${str}`;
 }
